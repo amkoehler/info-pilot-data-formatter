ac57d87ad304d855c8689bf6968202eb96c4bdac
Alex Koehler <alexkoeh@gmail.com>
2016-10-18 22:35:39 -0500

    Added some more column data per trial

--- InfoPilotDataFormatter.py
+++ InfoPilotDataFormatter.py
@@ -1,11 +1,5 @@
 import csv
 
-def first_lower(s):
-   if len(s) == 0:
-      return s
-   else:
-      return s[0].lower() + s[1:]
-
 with open('Info_PilotData.csv', 'rb') as f:
     reader = csv.reader(f)
     csvData = list(reader)
@@ -29,6 +23,7 @@ numParticipants = len(set(participants))
 print numParticipants
 
 trialColumnLabels = list([
+  'BoxSelection',
   'Candidate',
   'rt',
   'rtsq',
@@ -37,7 +32,8 @@ trialColumnLabels = list([
 ])
 
 columnHeadings = list([
-  'Participant'
+  'Participant',
+  'Condition'
 ])
 
 # Create column headings for each trial formatted as "TrialX.Field"
@@ -45,7 +41,7 @@ for i in range (0, int(maxTrials)):
   for j in range (0, len(trialColumnLabels)):
     columnHeadings.append('Trial' + str(i) + '.' + trialColumnLabels[j])
 
-formattedData = [['.' for i in range(len(columnHeadings))] for j in range(numParticipants + 1)]
+formattedData = [['.' for i in range(len(columnHeadings))] for j in range((numParticipants) * 3)]
 formattedData[0] = columnHeadings
 
 ###
@@ -53,21 +49,82 @@ formattedData[0] = columnHeadings
 ### parse through csvData and add the appropriate data to formattedData
 ###
 
-csvRow = 1 # Start at 1 since row 0 is the headings
-for i in range (1, numParticipants + 1):
+# Get all the column indices in csvData. This is more safe and flexible than assuming a
+# column is at a certain index but does assume that the column names in the original csv
+# do not change. 
+maxFlagIndex = csvData[0].index('max')
+participantIndex = csvData[0].index('participant')
+candidateIndex = csvData[0].index('candidate')
+conditionIndex = csvData[0].index('condition')
+rtIndex = csvData[0].index('rt')
+rtsqIndex = csvData[0].index('rtsq')
+candpidIndex = csvData[0].index('candpid')
+outpartyIndex = csvData[0].index('outparty')
 
-  formattedData[i][0] = csvData[csvRow][csvData[0].index('participant')]
-  numTrialsIdx = csvData[0].index('numtrials')
-  candidateIndex = csvData[0].index('candidate')
+# Box selection
+boxIndices = {
+  csvData[0].index('demogbox') : 'DemogBox',  
+  csvData[0].index('socissuebox') : 'SocIssueBox',  
+  csvData[0].index('econissuebox') : 'EconIssueBox',  
+  csvData[0].index('religbox') : 'ReligBox',  
+  csvData[0].index('scandalbox') : 'ScandalBox',  
+  csvData[0].index('personbox') : 'PersonBox',  
+  csvData[0].index('pictbox') : 'PictBox',  
+  csvData[0].index('viabilbox') : 'ViabilBox',  
+  csvData[0].index('experbox') : 'ExperBox',  
+  csvData[0].index('partyidbox') : 'PartyIDBox'
+}
 
-  # Per-trial data gets added here
-  for j in range (0, int(csvData[i][numTrialsIdx])):
+boxMinIndex = min(boxIndices.keys(), key=int)
+boxMaxIndex = max(boxIndices.keys(), key=int)
 
-    # Candidate
-    trialIdx = formattedData[0].index('Trial' + str(j) + '.Candidate')
-    formattedData[i][trialIdx] = csvData[csvRow][candidateIndex]
+fParticipantIndex = formattedData[0].index('Participant')
+fConditionIndex = formattedData[0].index('Condition')
+trialNum = 0
+x = 1
 
-    csvRow += 1
+for i in range (1, len(csvData)):
+
+  # Participant
+  if formattedData[x][participantIndex] == '.': 
+    formattedData[x][fParticipantIndex] = csvData[i][participantIndex]
+  
+  # Condition
+  if formattedData[x][conditionIndex] == '.': 
+    formattedData[x][fConditionIndex] = csvData[i][conditionIndex]
+
+  # Candidate
+  trialIdx = formattedData[0].index('Trial' + str(trialNum) + '.Candidate')
+  formattedData[x][trialIdx] = csvData[i][candidateIndex]
+
+  # rt
+  trialIdx = formattedData[0].index('Trial' + str(trialNum) + '.rt')
+  formattedData[x][trialIdx] = csvData[i][rtIndex]
+
+  # rtsq
+  trialIdx = formattedData[0].index('Trial' + str(trialNum) + '.rtsq')
+  formattedData[x][trialIdx] = csvData[i][rtsqIndex]
+
+  # CandPID
+  trialIdx = formattedData[0].index('Trial' + str(trialNum) + '.CandPID')
+  formattedData[x][trialIdx] = csvData[i][candpidIndex]
+
+  # OutParty
+  trialIdx = formattedData[0].index('Trial' + str(trialNum) + '.OutParty')
+  formattedData[x][trialIdx] = csvData[i][outpartyIndex]
+
+  # BoxSelection
+  trialIdx = formattedData[0].index('Trial' + str(trialNum) + '.BoxSelection')
+  for j in range (boxMinIndex, boxMaxIndex): 
+    if int(csvData[i][j]) == 1: 
+      formattedData[x][trialIdx] = boxIndices[j]
+
+  trialNum += 1
+
+  if int(csvData[i][maxFlagIndex]) == 1:
+    x += 1
+    trialNum = 0
+
 print formattedData
 ###
 ### List is now formatted correctly. Next step is to write list formattedData to a csv file.
